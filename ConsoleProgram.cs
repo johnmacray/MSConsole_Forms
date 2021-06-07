683a1c377ee4cec6f28aac697a9b5f3054854a36
johnmacray <johnmacray@internet.ru>
2021-06-07 21:01:44 +0300

    And delete two strange comments in the Page loop

--- ConsoleProgram.cs
+++ ConsoleProgram.cs
@@ -135,8 +135,6 @@ class Page
 		while(true)
 		{
 			key = Console.ReadKey(intercept: true);
-			//if (key.Key == ConsoleKey.UpArrow) Console.CursorTop--;
-			//if (key.Key == ConsoleKey.DownArrow) Console.CursorTop++;
 			pageNavigation();
 			keyboardEvents();
 		}
